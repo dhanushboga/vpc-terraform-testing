6e4a45b85c9e1b821f857baf8e8cc3125c6f8f4e
dhanushboga <dhanushboga555@gmail.com>
2025-02-17 23:02:03 +0530

    testing

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+# output "az_info" {
+#   value = module.vpc.az_info
+# }
+
+# output "subnets_info" {
+#   value = module.vpc.subnets_info
+# }
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,12 @@
-module "this" {
+module "vpc" {
   source = "../terraform-module-vpc"
   vpc_cidr = var.vpc_cidr
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
+  public_subnet_cidr = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  database_subnet_cidr = var.database_subnet_cidr
+  is_peering_required = true
 }
